dde80e11887188ba3e65b97f93e9e5ee66467056
Tycho Lursen <tycholursen@gmail.com>
2014-10-24 16:42:42 +0200

    Fix changed SI2168_TSMODE to SI2168_TS

--- drivers/media/pci/saa716x/saa716x_budget.c
+++ drivers/media/pci/saa716x/saa716x_budget.c
@@ -319,7 +319,7 @@ static int saa716x_tbs6281_frontend_attach(struct saa716x_adapter *adapter, int 
 	/* attach demod */
 	si2168_config.i2c_adapter = &i2cadapter;
 	si2168_config.fe = &adapter->fe;
-	si2168_config.ts_mode = SI2168_TSMODE_PARALLEL;
+	si2168_config.ts_mode = SI2168_TS_PARALLEL;
 	memset(&info, 0, sizeof(struct i2c_board_info));
 	strlcpy(info.type, "si2168", I2C_NAME_SIZE);
 	info.addr = 0x64;
@@ -408,7 +408,7 @@ static int saa716x_tbs6285_frontend_attach(struct saa716x_adapter *adapter, int 
 	/* attach demod */
 	si2168_config.i2c_adapter = &i2cadapter;
 	si2168_config.fe = &adapter->fe;
-	si2168_config.ts_mode = SI2168_TSMODE_SERIAL;
+	si2168_config.ts_mode = SI2168_TS_SERIAL;
 	memset(&info, 0, sizeof(struct i2c_board_info));
 	strlcpy(info.type, "si2168", I2C_NAME_SIZE);
 	info.addr = ((count == 0) || (count == 2)) ? 0x64 : 0x66;
